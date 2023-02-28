07cc3b0f90bd341ceacfb7b7ffafdb07ddabb00c
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2023-02-28 22:17:04 +0430

    add rspec test for posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:all) do
+      get user_posts_path(user_id: 1)
+    end
+
+    it 'returns http success' do
+      get '/users/show/posts/'
+      puts response
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) do
+      get user_post_path(user_id: 1, id: 1)
+    end
+
+    it 'returns http success' do
+      get '/users/show/posts/show'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http success' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'Render correct placeholder text' do
+      expect(response.body).to include('Here is a post for user')
+    end
   end
 end
+
