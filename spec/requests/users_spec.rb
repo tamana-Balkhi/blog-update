a760a14fea9e576603de554eebb7ffb1cee28d12
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2023-02-28 22:17:28 +0430

    add rspec test for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+describe 'GET /index' do
+  before(:each) do
+    get users_path
+  end
+
+  
+  it 'returns http success' do
+    get '/users/index'
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'Should Render the all user list' do
+    expect(response.body).to include('Here is a user index page')
+  end
+
+  it 'Render correct placeholder text' do
+    expect(response.body).to include('Here is a user index page')
   end
 end
+
+
+describe 'GET /show' do
+  before(:each) do
+    get user_path(id: 1)
+  end
+
+  it 'returns http success success' do
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'returns http success' do
+    expect(response).to render_template(:show)
+  end
+
+  it 'Render correct placeholder text' do
+    expect(response.body).to include('Here is user show page')
+  end
+end
+end
+
