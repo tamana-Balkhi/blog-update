8617cc4d405ff55367a30df5efaef398aec5fbd9
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2023-03-12 04:56:07 +0430

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  validates :name, presence: true
-  validates :postCounter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
-  validates :email, confirmation: { case_sensitive: false }
 
   def last_three_posts
     posts.order('created_at Desc').limit(3)
